a110c3249adc953f3a91e84f73e2667c69591fdc
Vaccummer <2316374406@qq.com>
2025-03-05 20:39:08 +0800

    25-03-05

---
+++ .history/setup_20250305203631.py
@@ -0,0 +1,31 @@
+from distutils.core import setup, Extension
+import pybind11
+import sys
+import os
+in_path = os.path.dirname(sys.executable)
+
+module = Extension(
+    'AMSFTP',
+    sources=[
+        'amsftp.cpp',
+    ],
+    include_dirs=[
+
+                    "C:\\Program Files (x86)\\Windows Kits\\10\\Include\\10.0.22000.0\\ucrt",
+                    "E:\\vcpkg\\installed\\x64-windows\\include",
+                    "C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\VC\\Tools\\MSVC\\14.43.34808\\atlmfc\\include",
+                    "C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\VC\\Tools\\MSVC\\14.43.34808\\include"
+    ] + pybind11.get_include(),
+    language='c++',
+    extra_compile_args=['/std:c++17', '/utf-8'],
+    #library_dirs=["E:\\vcpkg\\installed\\x64-windows\\lib", r'E:\Softwares\Anaconda\envs\Launcher\libs'],
+    library_dirs=["E:\\vcpkg\\installed\\x64-windows\\lib", r'E:\Softwares\Anaconda\envs\Launcher\libs'],
+    libraries=['libssh2', 'libcrypto', 'zlib', 'libssl', "ws2_32"]
+)
+
+setup(
+    name='AMSFTP',
+    version='0.1',
+    description='AMSCP, a python wrapper for AMSCPServer, SFTP Transfer with callback function',
+    ext_modules=[module]
+)
