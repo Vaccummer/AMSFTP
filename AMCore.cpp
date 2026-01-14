955fc51bbaf7b72a7643ae0a58151ecf076030c3
Vaccummer <2316374406@qq.com>
2026-01-14 19:36:30 +0800

    get some missing changes made yesterday

--- AMCore.cpp
+++ AMCore.cpp
@@ -1,9 +1,7 @@
 #include "AMCore.hpp"
 std::atomic<bool> is_wsa_initialized(false);
-void cleanup_wsa()
-{
-    if (is_wsa_initialized)
-    {
+void cleanup_wsa() {
+    if (is_wsa_initialized) {
         WSACleanup();
         is_wsa_initialized = false;
     }
