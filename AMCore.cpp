cda1f27fcbd5fc881457dc1fd8ee2d4b5a99c8de
Vaccummer <2316374406@qq.com>
2026-01-14 18:25:11 +0800

    use clang-format and adjust some function argument to reference type pass compile

--- AMCore.cpp
+++ AMCore.cpp
@@ -1,8 +1,10 @@
 #include "AMCore.hpp"
 std::atomic<bool> is_wsa_initialized(false);
-void cleanup_wsa() {
-  if (is_wsa_initialized) {
-    WSACleanup();
-    is_wsa_initialized = false;
-  }
+void cleanup_wsa()
+{
+    if (is_wsa_initialized)
+    {
+        WSACleanup();
+        is_wsa_initialized = false;
+    }
 }
