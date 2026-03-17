86484491731699db6cebf3ee93adeb5f277507f9
Vaccummer <2316374406@qq.com>
2026-03-17 14:30:29 +0800

    refactor TransferExecutionEngine, remove impl pattern, inherite ITransferExecutionPort remove chunk_size related function

--- include/infrastructure/client/runtime/TransferExecution.hpp
+++ include/infrastructure/client/runtime/TransferExecution.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "domain/client/ClientPort.hpp"
+#include "domain/transfer/TransferPort.hpp"
 #include "foundation/DataClass.hpp"
 
 #include <functional>
@@ -8,50 +9,33 @@
 
 namespace AMInfra::ClientRuntime {
 
-class TransferExecutionEngine {
+class TransferExecutionEngine final
+    : public AMDomain::transfer::ITransferExecutionPort,
+      NonCopyableNonMovable {
 public:
   using ECM = std::pair<ErrorCode, std::string>;
   using ClientHandle = std::shared_ptr<AMDomain::client::IClientPort>;
-  using ProgressCallback = std::function<void(const std::shared_ptr<TaskInfo> &,
+  using ProgressCallback = std::function<void(std::shared_ptr<TaskInfo>,
                                               WkProgressData &, bool force)>;
 
   /**
-   * @brief Construct a transfer execution engine with chunk size and progress
-   * callback.
+   * @brief Construct a transfer execution engine with progress callback.
    */
-  TransferExecutionEngine(size_t chunk_size = 256 * AMKB,
-                          ProgressCallback progress_callback = {});
+  explicit TransferExecutionEngine(ProgressCallback progress_callback = {});
 
   /**
    * @brief Destroy the execution engine implementation.
    */
-  ~TransferExecutionEngine();
-
-  TransferExecutionEngine(const TransferExecutionEngine &) = delete;
-  TransferExecutionEngine &operator=(const TransferExecutionEngine &) = delete;
-  TransferExecutionEngine(TransferExecutionEngine &&) noexcept;
-  TransferExecutionEngine &operator=(TransferExecutionEngine &&) noexcept;
-
-  /**
-   * @brief Set the transfer chunk size.
-   */
-  void SetChunkSize(size_t chunk_size);
-
-  /**
-   * @brief Get the current transfer chunk size.
-   */
-  size_t GetChunkSize() const;
+  ~TransferExecutionEngine() override;
 
   /**
    * @brief Execute one prepared single-file transfer.
    */
-  ECM ExecuteSingleFileTransfer(
-      const ClientHandle &src_client, const ClientHandle &dst_client,
-      const std::shared_ptr<TaskInfo> &task_info) const;
+  ECM TransferSignleFile(ClientHandle src_client, ClientHandle dst_client,
+                         std::shared_ptr<TaskInfo> task_info) const override;
 
 private:
-  class Impl;
-  std::unique_ptr<Impl> impl_;
+  ProgressCallback progress_callback_ = {};
 };
 
 } // namespace AMInfra::ClientRuntime
