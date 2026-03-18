a5878d11335028a90452cf01f508ae0461847d61
Vaccummer <2316374406@qq.com>
2026-03-18 12:37:09 +0800

    Refactor domain layer completely model: rearrange TaskInfo, move wprogressdata to infra layer service: deprecated all service, cache feature is aborted port: add ITransferPoolPort (actually original TaskWorker)

--- include/domain/transfer/TransferPort.hpp
+++ include/domain/transfer/TransferPort.hpp
@@ -1,38 +1,66 @@
 #pragma once
 
+#include "domain/transfer/TransferDomainModel.hpp"
 #include "foundation/DataClass.hpp"
-#include <functional>
 #include <memory>
-#include <utility>
+#include <unordered_map>
 
 namespace AMDomain::client {
 class IClientPort;
 }
 
 namespace AMDomain::transfer {
-/**
- * @brief Domain-level runtime abstraction for single-file transfer execution.
- */
-class ITransferExecutionPort {
+
+class ITransferPoolPort {
 public:
-  using ECM = std::pair<ErrorCode, std::string>;
-  using ClientHandle = std::shared_ptr<AMDomain::client::IClientPort>;
-  using ProgressCallback =
-      std::function<void(std::shared_ptr<TaskInfo>, WkProgressData &, bool)>;
+  virtual ~ITransferPoolPort() = default;
 
-  virtual ~ITransferExecutionPort() = default;
+  virtual ECM Shutdown(int timeout_ms = 5000) = 0;
 
-  /**
-   * @brief Execute one single-file transfer item.
-   */
-  [[nodiscard]] virtual ECM
-  TransferSignleFile(ClientHandle src_client, ClientHandle dst_client,
-                     std::shared_ptr<TaskInfo> task_info) const = 0;
+  virtual size_t ThreadCount(size_t new_count = 0) = 0;
+
+  [[nodiscard]] virtual std::unordered_map<size_t, bool>
+  GetThreadIDs() const = 0;
+
+  virtual ECM Submit(std::shared_ptr<TaskInfo> task_info,
+                     TransferClientContainer clients) = 0;
+
+  [[nodiscard]] virtual std::optional<TaskStatus>
+  GetStatus(const TaskInfo::ID &id) const = 0;
+
+  virtual std::shared_ptr<TaskInfo> GetResultTask(const TaskInfo::ID &id,
+                                                  bool remove = true) = 0;
+
+  [[nodiscard]] virtual std::shared_ptr<TaskInfo>
+  GetActiveTask(const TaskInfo::ID &id) const = 0;
+
+  [[nodiscard]] virtual std::unordered_map<TaskInfo::ID,
+                                           std::shared_ptr<TaskInfo>>
+  GetAllActiveTasks() const = 0;
+
+  [[nodiscard]] virtual std::unordered_map<TaskInfo::ID,
+                                           std::shared_ptr<TaskInfo>>
+  GetPendingTasks() const = 0;
+
+  [[nodiscard]] virtual std::unordered_map<TaskInfo::ID,
+                                           std::shared_ptr<TaskInfo>>
+  GetConductingTasks() const = 0;
+
+  [[nodiscard]] virtual std::unordered_map<TaskInfo::ID,
+                                           std::shared_ptr<TaskInfo>>
+  GetAllHistoryTasks() const = 0;
+
+  virtual void ClearResults() = 0;
+
+  virtual bool RemoveResult(const TaskInfo::ID &id) = 0;
+
+  virtual ECM Pause(const TaskInfo::ID &id, int timeout_ms = 5000) = 0;
+
+  virtual ECM Resume(const TaskInfo::ID &id, int timeout_ms = 5000) = 0;
+
+  virtual std::pair<std::shared_ptr<TaskInfo>, ECM>
+  Terminate(const TaskInfo::ID &id, int timeout_ms = 5000) = 0;
 };
 
-/**
- * @brief Create default transfer execution port backed by infrastructure.
- */
-std::unique_ptr<ITransferExecutionPort> CreateDefaultTransferExecutionPort(
-    ITransferExecutionPort::ProgressCallback callback);
+std::unique_ptr<ITransferPoolPort> CreateTransferPoolPort();
 } // namespace AMDomain::transfer
