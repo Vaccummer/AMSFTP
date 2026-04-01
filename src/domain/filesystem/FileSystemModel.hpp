d7287addba9802a19e871fee48786f305dff007a
Vaccummer <2316374406@qq.com>
2026-04-01 15:26:28 +0800

    refactor(filesystem): introduce PathTarget/ResolvedPath and use target-based cache keys

--- src/domain/filesystem/FileSystemModel.hpp
+++ src/domain/filesystem/FileSystemModel.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include "domain/client/ClientPort.hpp"
 #include "domain/host/HostModel.hpp"
+#include <optional>
+#include <utility>
 
 namespace AMDomain::filesystem {
 using ClientNickname = AMDomain::host::ConRequest::ClientNickname;
@@ -10,17 +12,35 @@ struct FilesystemArg {
   int max_cd_history = 1;
 };
 
+struct PathTarget {
+  ClientNickname nickname = "";
+  std::string path = "";
+};
+
+struct ResolvedPath {
+  PathTarget target = {};
+  std::string abs_path = "";
+  ClientHandle client = nullptr;
+  bool is_wildcard = false;
+  bool is_user_path = false;
+};
+
+struct PathEntry {
+  ResolvedPath resolved = {};
+  PathInfo info = {};
+};
+
 struct ClientPath {
   ClientNickname nickname = "";
   std::string path = "";
+  std::optional<PathInfo> info = std::nullopt;
   ClientHandle client = nullptr;
-  ECM rcm = ECM{EC::Success, ""};
   bool is_wildcard = false;
   bool userpath = false; // true if the path is a userpath (e.g. ~/foo)
   bool resolved = false;
+  ECM rcm = ECM{EC::Success, ""};
 };
 
 using StatPathResult = std::pair<ECM, PathInfo>;
 using RttQueryResult = std::pair<ECM, double>;
-
 } // namespace AMDomain::filesystem
